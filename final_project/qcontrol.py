ec1b16d4e04ee9cc492b92896b4052a82fc366a9
Eli Weissler <eliweissler@gmail.com>
2023-12-12 18:02:59 -0700

    ready to view

--- final_project/qcontrol.py
+++ final_project/qcontrol.py
@@ -40,6 +40,8 @@ class QuantumEnv(EnvBase):
     Environment for doing reinforcement learning with
     schrodinger dynamics
 
+    Adapted from https://pytorch.org/rl/tutorials/pendulum.html
+
     Mandatory Methods:
      _step
      _reset
@@ -400,197 +402,197 @@ if __name__ == "__main__":
     torch.manual_seed(0)
     env.set_seed(0)
 
-    class myNet(nn.Module):
+    # class myNet(nn.Module):
 
-        def __init__(self):
-            super(myNet, self).__init__()
-            ctype = torch.float32
-            p_drop = 0.01
-            bias = True
-            self.net = nn.Sequential(
-                        nn.Linear(2*psi_0.size + 1, 64, dtype=ctype, bias=bias),
-                        nn.Tanh(),
-                        # nn.Dropout(p=p_drop),
-                        nn.Linear(64, 64, dtype=ctype, bias=bias),
-                        nn.Tanh(),
-                        nn.Dropout(p=p_drop),
-                        nn.Linear(64, 16, dtype=ctype, bias=bias),
-                        nn.Tanh(),
-                        nn.Dropout(p=p_drop),
-                        nn.Linear(16, 1, dtype=ctype, bias=bias),
-                        # nn.Tanh()
-                        )
+    #     def __init__(self):
+    #         super(myNet, self).__init__()
+    #         ctype = torch.float32
+    #         p_drop = 0.01
+    #         bias = True
+    #         self.net = nn.Sequential(
+    #                     nn.Linear(2*psi_0.size + 1, 64, dtype=ctype, bias=bias),
+    #                     nn.Tanh(),
+    #                     # nn.Dropout(p=p_drop),
+    #                     nn.Linear(64, 64, dtype=ctype, bias=bias),
+    #                     nn.Tanh(),
+    #                     nn.Dropout(p=p_drop),
+    #                     nn.Linear(64, 16, dtype=ctype, bias=bias),
+    #                     nn.Tanh(),
+    #                     nn.Dropout(p=p_drop),
+    #                     nn.Linear(16, 1, dtype=ctype, bias=bias),
+    #                     # nn.Tanh()
+    #                     )
 
-        def forward(self, x1, x2, x3):
-            # print(x1.shape, x2.shape)
-            # flattened = torch.concatenate((x1.flatten(), x2.flatten()))
-            # flattened = 1.0j*torch.hstack((x1, x2)).squeeze()
-            flattened = torch.hstack((x1.flatten(1, -1), x2.flatten(1, -1), x3.reshape(-1,1)))
-            # ctype = torch.float32
-            # self.net = nn.Sequential(
-            #             nn.Linear(4, 64, dtype=ctype),
-            #             nn.Tanh(),
-            #             nn.Linear(64, 32, dtype=ctype),
-            #             nn.Tanh(),
-            #             nn.Linear(32, 16, dtype=ctype),
-            #             nn.Tanh(),
-            #             nn.Linear(16, 1, dtype=ctype),
-            #             # nn.Tanh()
-            #             )
-            # x = torch.hstack((x1, x2)).squeeze()
-            # x = nn.Linear(4, 64, dtype=ctype)(x)
-            # x = nn.Tanh(x)
-            # x = torch.clamp(self.net(flattened), min=-control_thresh, max=control_thresh)
-            return self.net(flattened)
-        # @staticmethod
-        # def dropout_complex(x):
-        #     # work around unimplemented dropout for complex
-        #     if x.is_complex():
-        #         mask = torch.nn.functional.dropout(torch.ones_like(x.real))
-        #         return x * mask
-        #     else:
-        #         return torch.nn.functional.dropout(x)
+    #     def forward(self, x1, x2, x3):
+    #         # print(x1.shape, x2.shape)
+    #         # flattened = torch.concatenate((x1.flatten(), x2.flatten()))
+    #         # flattened = 1.0j*torch.hstack((x1, x2)).squeeze()
+    #         flattened = torch.hstack((x1.flatten(1, -1), x2.flatten(1, -1), x3.reshape(-1,1)))
+    #         # ctype = torch.float32
+    #         # self.net = nn.Sequential(
+    #         #             nn.Linear(4, 64, dtype=ctype),
+    #         #             nn.Tanh(),
+    #         #             nn.Linear(64, 32, dtype=ctype),
+    #         #             nn.Tanh(),
+    #         #             nn.Linear(32, 16, dtype=ctype),
+    #         #             nn.Tanh(),
+    #         #             nn.Linear(16, 1, dtype=ctype),
+    #         #             # nn.Tanh()
+    #         #             )
+    #         # x = torch.hstack((x1, x2)).squeeze()
+    #         # x = nn.Linear(4, 64, dtype=ctype)(x)
+    #         # x = nn.Tanh(x)
+    #         # x = torch.clamp(self.net(flattened), min=-control_thresh, max=control_thresh)
+    #         return self.net(flattened)
+    #     # @staticmethod
+    #     # def dropout_complex(x):
+    #     #     # work around unimplemented dropout for complex
+    #     #     if x.is_complex():
+    #     #         mask = torch.nn.functional.dropout(torch.ones_like(x.real))
+    #     #         return x * mask
+    #     #     else:
+    #     #         return torch.nn.functional.dropout(x)
 
-    net = myNet().to(device)
-    policy = TensorDictModule(
-        net,
-        in_keys=["psi_real", "psi_imag", "t_left"],
-        out_keys=["action"]
-    )
-    optim = torch.optim.Adam(policy.parameters(), lr=2e-3)
+    # net = myNet().to(device)
+    # policy = TensorDictModule(
+    #     net,
+    #     in_keys=["psi_real", "psi_imag", "t_left"],
+    #     out_keys=["action"]
+    # )
+    # optim = torch.optim.Adam(policy.parameters(), lr=2e-3)
 
-    # Train Parameters
-    batch_size = 100
-    rollout_len = int(gate_time/dt)
-    total_trials = 10000//2
-    # total_trials = 50
-    pbar = tqdm.tqdm(range(total_trials // batch_size))
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, total_trials)
-    # scheduler = torch.optim.lr_scheduler.LinearLR(optim, total_iters=total_trials)
-    logs = defaultdict(list)
+    # # Train Parameters
+    # batch_size = 100
+    # rollout_len = int(gate_time/dt)
+    # total_trials = 10000//2
+    # # total_trials = 50
+    # pbar = tqdm.tqdm(range(total_trials // batch_size))
+    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optim, total_trials)
+    # # scheduler = torch.optim.lr_scheduler.LinearLR(optim, total_iters=total_trials)
+    # logs = defaultdict(list)
 
-    # Keep track of the best run throughout training
-    logs["best_run"] = {}
-    logs["best_run"]["fidelity"] = 0
-    logs["best_run"]["reward"] = -np.inf
-    logs["best_run"]["control"] = None
-    logs["best_run"]["psi"] = None
+    # # Keep track of the best run throughout training
+    # logs["best_run"] = {}
+    # logs["best_run"]["fidelity"] = 0
+    # logs["best_run"]["reward"] = -np.inf
+    # logs["best_run"]["control"] = None
+    # logs["best_run"]["psi"] = None
 
-    params = env.gen_params(batch_size=[batch_size], dt=dt,
-                            gate_time=gate_time, control_thresh=control_thresh,
-                            control_penalty=control_penalty, psi_0=psi_0,
-                            psi_f=psi_f, H0=H0, H1=H1, close_thresh=close_thresh)
-    for _ in pbar:
-        init_td = env.reset(params)
-        rollout = env.rollout(rollout_len, policy, tensordict=init_td, auto_reset=False)
-        traj_return = rollout["next", "reward"].mean()
-        (-traj_return).backward()
-        gn = torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
-        optim.step()
-        optim.zero_grad()
-        pbar.set_description(
-            f"reward: {traj_return: 4.4f}, "
-            f"last reward: {rollout[..., -1]['next', 'reward'].mean(): 4.4f}, gradient norm: {gn: 4.4}"
-        )
-        logs["return"].append(traj_return.item())
-        logs["last_reward"].append(rollout[..., -1]["next", "reward"].mean().item())
+    # params = env.gen_params(batch_size=[batch_size], dt=dt,
+    #                         gate_time=gate_time, control_thresh=control_thresh,
+    #                         control_penalty=control_penalty, psi_0=psi_0,
+    #                         psi_f=psi_f, H0=H0, H1=H1, close_thresh=close_thresh)
+    # for _ in pbar:
+    #     init_td = env.reset(params)
+    #     rollout = env.rollout(rollout_len, policy, tensordict=init_td, auto_reset=False)
+    #     traj_return = rollout["next", "reward"].mean()
+    #     (-traj_return).backward()
+    #     gn = torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
+    #     optim.step()
+    #     optim.zero_grad()
+    #     pbar.set_description(
+    #         f"reward: {traj_return: 4.4f}, "
+    #         f"last reward: {rollout[..., -1]['next', 'reward'].mean(): 4.4f}, gradient norm: {gn: 4.4}"
+    #     )
+    #     logs["return"].append(traj_return.item())
+    #     logs["last_reward"].append(rollout[..., -1]["next", "reward"].mean().item())
 
-        psi_final = rollout["psi_real"][:, -1] + 1.0j*rollout["psi_imag"][:, -1]
-        F_final = QuantumEnv.avg_fidelity(params["params","psi_f"], psi_final)
-        ibest = torch.argmax(F_final)
-        rbest = rollout["next", "reward"][ibest, -1]
-        fbest = F_final[ibest]
-        if fbest > logs["best_run"]["fidelity"]:
-            logs["best_run"]["reward"] = rbest
-            logs["best_run"]["psi"] = rollout["psi_real"][ibest] + 1.0j*rollout["psi_imag"][ibest]
-            logs["best_run"]["control"] = rollout["action"][ibest]
-            logs["best_run"]["fidelity"] = fbest
+    #     psi_final = rollout["psi_real"][:, -1] + 1.0j*rollout["psi_imag"][:, -1]
+    #     F_final = QuantumEnv.avg_fidelity(params["params","psi_f"], psi_final)
+    #     ibest = torch.argmax(F_final)
+    #     rbest = rollout["next", "reward"][ibest, -1]
+    #     fbest = F_final[ibest]
+    #     if fbest > logs["best_run"]["fidelity"]:
+    #         logs["best_run"]["reward"] = rbest
+    #         logs["best_run"]["psi"] = rollout["psi_real"][ibest] + 1.0j*rollout["psi_imag"][ibest]
+    #         logs["best_run"]["control"] = rollout["action"][ibest]
+    #         logs["best_run"]["fidelity"] = fbest
 
-        scheduler.step()
+    #     scheduler.step()
 
-    timesteps = rollout_len
-    tlist = np.arange(timesteps)*dt
+    # timesteps = rollout_len
+    # tlist = np.arange(timesteps)*dt
 
-    pulse = logs["best_run"]["control"].cpu().detach().numpy()
-    psi_hist = logs["best_run"]["psi"].cpu().detach().numpy()
-    fid = logs["best_run"]["fidelity"].cpu().detach().numpy()
-    len_pulse = pulse.size
+    # pulse = logs["best_run"]["control"].cpu().detach().numpy()
+    # psi_hist = logs["best_run"]["psi"].cpu().detach().numpy()
+    # fid = logs["best_run"]["fidelity"].cpu().detach().numpy()
+    # len_pulse = pulse.size
 
-    prob0_start0 = [np.abs(s[0, 0])**2 for s in psi_hist]
-    prob0_start1 = [np.abs(s[0, 1])**2 for s in psi_hist]
+    # prob0_start0 = [np.abs(s[0, 0])**2 for s in psi_hist]
+    # prob0_start1 = [np.abs(s[0, 1])**2 for s in psi_hist]
 
-    f, ax = plt.subplots(ncols=2)
-    f.set_size_inches((8, 3))
-    ax[0].set_title(f"pulse (best) - fidelity: {str(np.round(fid, 6))}")
-    ax[1].set_title("population in [1,0] state")
-    ax[0].plot(tlist[:len_pulse], pulse)
-    ax[1].plot(tlist[:len_pulse], prob0_start0, label="starts in [1,0]")
-    ax[1].plot(tlist[:len_pulse], prob0_start1, label="starts in [0,1]")
-    ax[1].legend()
-    plt.savefig("pulse_best.png")
-
-
-    def plot():
-        with plt.ion():
-            plt.figure(figsize=(10, 5))
-            plt.subplot(1, 2, 1)
-            plt.plot(logs["return"])
-            plt.title("returns")
-            plt.xlabel("iteration")
-            plt.subplot(1, 2, 2)
-            plt.plot(logs["last_reward"])
-            plt.title("last reward")
-            plt.xlabel("iteration")
-            if is_ipython:
-                display.display(plt.gcf())
-                display.clear_output(wait=True)
-            plt.savefig("training.png")
-
-    plot()
+    # f, ax = plt.subplots(ncols=2)
+    # f.set_size_inches((8, 3))
+    # ax[0].set_title(f"pulse (best) - fidelity: {str(np.round(fid, 6))}")
+    # ax[1].set_title("population in [1,0] state")
+    # ax[0].plot(tlist[:len_pulse], pulse)
+    # ax[1].plot(tlist[:len_pulse], prob0_start0, label="starts in [1,0]")
+    # ax[1].plot(tlist[:len_pulse], prob0_start1, label="starts in [0,1]")
+    # ax[1].legend()
+    # plt.savefig("pulse_best.png")
 
 
-    def get_control_pulse(net, params, timesteps=timesteps, dt=dt, ctype=torch.complex64):
+    # def plot():
+    #     with plt.ion():
+    #         plt.figure(figsize=(10, 5))
+    #         plt.subplot(1, 2, 1)
+    #         plt.plot(logs["return"])
+    #         plt.title("returns")
+    #         plt.xlabel("iteration")
+    #         plt.subplot(1, 2, 2)
+    #         plt.plot(logs["last_reward"])
+    #         plt.title("last reward")
+    #         plt.xlabel("iteration")
+    #         if is_ipython:
+    #             display.display(plt.gcf())
+    #             display.clear_output(wait=True)
+    #         plt.savefig("training.png")
 
-        net.eval()
-        psi = params["params"]["psi_0"][0] # torch.tensor(psi_0, dtype=ctype, device=device)
-        t_left = torch.tensor(1, device=device)
-        t_increment = params["params"]["dt"][0]/params["params"]["gate_time"][0]
-        H0_tensor = params["params"]["H0"][0]#torch.tensor(H0, dtype=ctype, device=device)
-        H1_tensor = params["params"]["H1"][0]#torch.tensor(H1, dtype=ctype, device=device)
-        psi_f = params["params"]["psi_f"][0]
-        cntrl_seq = torch.zeros(timesteps, dtype=ctype, device=device)
-        fid = torch.zeros(timesteps, device=device)
-        psi_list = []
-        for i in range(timesteps):
-            cntrl = net(torch.real(psi).reshape((1,4,1)), torch.imag(psi).reshape((1,4,1)), t_left)
-            cntrl_seq[i] = cntrl
-            U = torch.linalg.matrix_exp(-1.0j*dt*(H0_tensor+cntrl*H1_tensor))
-            psi = U@psi
-            fid[i] = QuantumEnv.avg_fidelity(psi_f, psi)
-            psi_list.append(psi.cpu().detach().numpy())
-            t_left = t_left - t_increment
+    # plot()
 
-        return cntrl_seq.cpu().detach().numpy(), psi_list, fid.cpu().detach().numpy()
 
-    pulse, psi_list, fid = get_control_pulse(net, params)
+    # def get_control_pulse(net, params, timesteps=timesteps, dt=dt, ctype=torch.complex64):
 
-    def Ham(t, tlist):
-        tdiff = np.abs(tlist - t)
-        return qt.Qobj(H0 + pulse[np.argmin(tdiff)]*H1)
+    #     net.eval()
+    #     psi = params["params"]["psi_0"][0] # torch.tensor(psi_0, dtype=ctype, device=device)
+    #     t_left = torch.tensor(1, device=device)
+    #     t_increment = params["params"]["dt"][0]/params["params"]["gate_time"][0]
+    #     H0_tensor = params["params"]["H0"][0]#torch.tensor(H0, dtype=ctype, device=device)
+    #     H1_tensor = params["params"]["H1"][0]#torch.tensor(H1, dtype=ctype, device=device)
+    #     psi_f = params["params"]["psi_f"][0]
+    #     cntrl_seq = torch.zeros(timesteps, dtype=ctype, device=device)
+    #     fid = torch.zeros(timesteps, device=device)
+    #     psi_list = []
+    #     for i in range(timesteps):
+    #         cntrl = net(torch.real(psi).reshape((1,4,1)), torch.imag(psi).reshape((1,4,1)), t_left)
+    #         cntrl_seq[i] = cntrl
+    #         U = torch.linalg.matrix_exp(-1.0j*dt*(H0_tensor+cntrl*H1_tensor))
+    #         psi = U@psi
+    #         fid[i] = QuantumEnv.avg_fidelity(psi_f, psi)
+    #         psi_list.append(psi.cpu().detach().numpy())
+    #         t_left = t_left - t_increment
 
-    # states_qt = qt.sesolve(lambda t, args: Ham(t, tlist), qt.Qobj(psi_0), tlist=tlist)
-    # prob0_qt = [np.abs(s[0][0][0])**2 for s in states_qt.states]
+    #     return cntrl_seq.cpu().detach().numpy(), psi_list, fid.cpu().detach().numpy()
 
-    prob0_start0 = [np.abs(s[0,0])**2 for s in psi_list]
-    prob0_start1 = [np.abs(s[0,1])**2 for s in psi_list]
+    # pulse, psi_list, fid = get_control_pulse(net, params)
 
-    f, ax = plt.subplots(ncols=2)
-    f.set_size_inches((8, 3))
-    ax[0].set_title(f"pulse (from network) - F: {str(np.round(fid[-1], 6))}")
-    ax[1].set_title("population in [1,0] state")
-    ax[0].plot(tlist, pulse)
-    # ax[1].plot(tlist, prob0_qt, label="qutip")
-    ax[1].plot(tlist, prob0_start0, label="starts in [1,0]")
-    ax[1].plot(tlist, prob0_start1, label="starts in [0,1]")
-    ax[1].legend()
-    plt.savefig("pulse.png")
+    # def Ham(t, tlist):
+    #     tdiff = np.abs(tlist - t)
+    #     return qt.Qobj(H0 + pulse[np.argmin(tdiff)]*H1)
+
+    # # states_qt = qt.sesolve(lambda t, args: Ham(t, tlist), qt.Qobj(psi_0), tlist=tlist)
+    # # prob0_qt = [np.abs(s[0][0][0])**2 for s in states_qt.states]
+
+    # prob0_start0 = [np.abs(s[0,0])**2 for s in psi_list]
+    # prob0_start1 = [np.abs(s[0,1])**2 for s in psi_list]
+
+    # f, ax = plt.subplots(ncols=2)
+    # f.set_size_inches((8, 3))
+    # ax[0].set_title(f"pulse (from network) - F: {str(np.round(fid[-1], 6))}")
+    # ax[1].set_title("population in [1,0] state")
+    # ax[0].plot(tlist, pulse)
+    # # ax[1].plot(tlist, prob0_qt, label="qutip")
+    # ax[1].plot(tlist, prob0_start0, label="starts in [1,0]")
+    # ax[1].plot(tlist, prob0_start1, label="starts in [0,1]")
+    # ax[1].legend()
+    # plt.savefig("pulse.png")
